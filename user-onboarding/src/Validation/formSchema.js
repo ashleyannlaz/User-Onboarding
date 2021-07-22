e7e5adf302fee91e0b0d5b09646215b28630f72c
ashleyannlaz <paperpastels@gmail.com>
2021-07-21 21:30:10 -0400

    Checkbox must be selected

--- user-onboarding/src/Validation/formSchema.js
+++ user-onboarding/src/Validation/formSchema.js
@@ -1,10 +1,23 @@
 import * as yup from  'yup'
 
 const formSchema = yup.object().shape({
-    username: yup.string().trim().required('Username is required').min(5, 'Username must be at least 5 characters long'),
-    email: yup.string().email('Must be a valid email address').required('Email is required'),
-    password: yup.string().trim().required('Password is required').min(5, 'Password must be at least 5 characters long'),
-    tos: yup.boolean(),
+    username: 
+    yup.string()
+    .trim()
+    .required('Username is required')
+    .min(3, 'Username must be at least 3 characters long'),
+    email: 
+    yup.string()
+    .email('Must be a valid email address')
+    .required('Email is required'),
+    password: 
+    yup.string()
+    .trim()
+    .required('Password is required')
+    .min(5, 'Password must be at least 5 characters long'),
+    tos: 
+    yup.boolean()
+    .oneOf([true], 'Must be selected.'),
 })
 
 export default formSchema
